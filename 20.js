2ef05f7705394ec910b8c13031c9b3007fd96e69
Anton Vlasik <snumber29@gmail.com>
2018-11-10 19:46:26 +0300

    factorization

--- 20.js
+++ 20.js
@@ -5,3 +5,48 @@ function fact(n) {
     }
     return result;
 }
+
+function factorization(n) {
+    const sieve = findPrimes_sieve(n);
+    const data = [];
+    for (let i = 1; i < n; i++) {
+        if (sieve[i]) {
+            data.push(i);
+        } else {
+            data.push(...factorize(i));
+        }
+    }
+    console.log(fact(n));
+    console.log(data.reduce((res, el) => res * el, 1));
+    return data;
+}
+
+function factorize(number) {
+    let divisor = 2;
+    const divisors = [];
+    while (number > 1) {
+        if (number % divisor === 0) {
+            number /= divisor;
+            divisors.push(divisor);
+            divisor--;
+        }
+        divisor++;
+    }
+    return divisors;
+}
+
+function findPrimes_sieve(number) {
+    const S = [];
+    S[1] = 0;
+    for (let k = 2; k <= number; k++)
+        S[k] = 1;
+
+    for (let k = 2; k * k <= number; k++) {
+        if (S[k] === 1) {
+            for (let l = k * k; l <= number; l += k) {
+                S[l] = 0;
+            }
+        }
+    }
+    return S;
+}
--- _measurePerformance.js
+++ _measurePerformance.js
@@ -1,9 +1,9 @@
 function measurePerformance(...args) {
     let result;
-    const isTimesOmmited = typeof args[0] === 'function';
-    const times = isTimesOmmited ? 1 : args[0];
-    const func = isTimesOmmited ? args[0] : args[1];
-    const functionArgs = args.slice(isTimesOmmited ? 1 : 2);
+    const isTimesOmitted = typeof args[0] === 'function';
+    const times = isTimesOmitted ? 1 : args[0];
+    const func = isTimesOmitted ? args[0] : args[1];
+    const functionArgs = args.slice(isTimesOmitted ? 1 : 2);
     const start = performance.now();
     for (let i = 0; i < times; i++) {
         result = func(...functionArgs)
